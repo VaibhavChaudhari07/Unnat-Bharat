01a4bdc8751c125f3e025ecef79c73a6c6c7994c
VaibhavChaudhari07 <chaudharivaibhav066@gmail.com>
2024-08-31 15:51:24 +0530

    updated

--- server.js
+++ server.js
@@ -7,23 +7,30 @@ import authRoutes from "./routes/authRoute.js";
 import formRoutes from "./routes/formRoutes.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 
+// Configure environment variables
 dotenv.config();
 
+// Connect to the database
 connectDB();
 
 const app = express();
 
+// Middleware setup
 app.use(cors());
 app.use(express.json());
 
+// Route setup
 app.use("/api/v1/auth", authRoutes);
-
 app.use("/api/v1/send", formRoutes);
 
-// app.use("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+// Handle serving the frontend build files in production
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 const PORT = process.env.PORT || 8080;
 
